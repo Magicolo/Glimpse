165e959e5aa54816e03fcdcaa66cde9e43fab4a3
jlocas <jeremie.locas@hotmail.com>
2014-11-12 13:21:49 -0500

    started area triggers JL

---
+++ Assets/Scripts/Audio/AreaTriggers.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using LibPDBinding;
+
+public class AreaTriggers : MonoBehaviour {
+
+	GameObject trigger1;
+
+	public int area = 0;
+	int[] areas = new int[2];
+
+	void OnTriggerEnter(Collider trigger1){
+		Debug.Log ("caca enteredd");
+		}
+
+	// Use this for initialization
+	void Start () {
+		trigger1 = GameObject.Find("LevelProgress/Trigger1");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
