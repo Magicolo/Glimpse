cda59805bf08f0ea2b2ad1420d42b79136ce8da6
straty91 <straty91@hotmail.com>
2014-11-12 13:16:32 -0500

    Piano is working!

--- Assets/Scripts/Audio/SamplePlayer.cs
+++ Assets/Scripts/Audio/SamplePlayer.cs
@@ -1,20 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using System.Collections;
 using LibPDBinding;
 
 public class SamplePlayer : MonoBehaviour {
 
+	readonly List<int> notesToPlay = new List<int>();
+	readonly List<float> velocitiesToPlay = new List<float>();
+	
 	void Awake(){
+		LibPD.Subscribe("music_piano_midi");
 		LibPD.List += ReceiveList;
+	}
+	
+	void Update(){
+		for (int i = 0; i < notesToPlay.Count; i++) {
+			Sampler.Play("Piano", notesToPlay.Pop(), velocitiesToPlay.Pop());
+		}
 	}
 	
 	void OnApplicationQuit(){
 		LibPD.List -= ReceiveList;
+		LibPD.Unsubscribe("music_piano_midi");
 	}
 	
 	void ReceiveList(string sendName, object[] values) {
 		if (sendName == "music_piano_midi"){
-			Sampler.Play("Piano", (int)values[0], (float)values[1]);
+			notesToPlay.Add((int)(float)values[0]);
+			velocitiesToPlay.Add((float)values[1]);
 		}
 	}
 }
--- Assets/Scripts/Audio/Wind.cs
+++ Assets/Scripts/Audio/Wind.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 [System.Serializable]
-public class WindParameters {
+public class Wind {
 
 	public GlimpseCamera glimpseCam;
 
@@ -320,6 +320,7 @@ public class WindParameters {
 			Update();
 		}
 	}
+	
 	//windspeed
 	[SerializeField, PropertyField]
 	float min;
@@ -333,13 +334,7 @@ public class WindParameters {
 		}
 	}
 	
-
-
-
-		
-	
 	public void Update() {
-
 		if (Application.isPlaying) {
 			PDPlayer.SendValue("wind_BG_freqBase", Wind_BG_freqBase);
 			PDPlayer.SendValue("wind_BG_freqAmbitus", Wind_BG_freqAmbitus);
@@ -366,10 +361,8 @@ public class WindParameters {
 			PDPlayer.SendValue("wind_WH_randVal", Wind_WH_randVal);
 			PDPlayer.SendValue("wind_leaves_mul", Wind_leaves_mul);
 			PDPlayer.SendValue("wind_reverb_dry-wet", Wind_reverb_dryWet);
-		}
 
-		//windspeed
-		if (Application.isPlaying) {
+			//windspeed
 			PDPlayer.SendValue("wind_speed_max", Max);
 			PDPlayer.SendValue("wind_speed_min", Min);
 		}
