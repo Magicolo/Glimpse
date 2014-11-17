9c599e7cd977ef7834076daf77baee1a42d302b8
jlocas <jeremie.locas@hotmail.com>
2014-11-16 23:41:09 -0500

    sound fixes

--- Assets/Scripts/Audio/SoundClips.cs
+++ Assets/Scripts/Audio/SoundClips.cs
@@ -5,10 +5,11 @@ public class SoundClips : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		AudioPlayer.Play("AMB_Pond_WaterfallLoop", GameObject.Find ("Waterfall_Audio_A_01"));	
+		/*AudioSource.PlayClipAtPoint(AMB_Pond_WaterfallLoop, GameObject.Find ("Waterfall_Audio_A_01").transform.position);
+		/*AudioPlayer.Play("AMB_Pond_WaterfallLoop", GameObject.Find ("Waterfall_Audio_A_01"));	
 		AudioPlayer.Play("AMB_Pond_WaterfallLoop_02", GameObject.Find ("Waterfall_Audio_B_01"));
 		AudioPlayer.Play("AMB_Pond_WaterfallLoop", GameObject.Find ("Waterfall_Audio_A_02"));	
-		AudioPlayer.Play("AMB_Pond_WaterfallLoop_02", GameObject.Find ("Waterfall_Audio_B_02"));
+		AudioPlayer.Play("AMB_Pond_WaterfallLoop_02", GameObject.Find ("Waterfall_Audio_B_02"));*/
 		//AudioPlayer.Play("AMB_Pond_WavesLoop", GameObject.Find ("Waves"));
 	}
 	
